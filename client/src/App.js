71a437983ddd011ba93795795379f12d090cba13
bethanyshoun <bethanyshoun@gmail.com>
2022-09-25 17:51:50 -0400

    create concurrency

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import React from 'react';
 
 import Header from './components/Header';
@@ -5,15 +6,27 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+//URI stands for Uniform Resource Identifier
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
